499d5c919922174a4d906727063fa3f966ffc9e3
Konrad Gnat <konradmgnat@gmail.com>
2022-06-04 15:46:31 -0700

    contracts

--- contracts/Cohort.sol
+++ contracts/Cohort.sol
@@ -6,10 +6,72 @@ contract Cohort {
     uint256 public id;
     string public name;
     string public mission;
+    address public dao;
+    address[] public members;
+    mapping(address => uint256) memberJoinDate;
+
+    enum Status {
+        Open,
+        Completed,
+        Incomplete
+    }
+
+    struct Session {
+        string date;
+        Status status;
+        string reflection;
+        address[] attestations;
+    }
 
     constructor(uint256 _id, string memory _name, string memory _mission) {
         id = _id;
         name = _name;
         mission = _mission;
+        dao = msg.sender;
+    }
+
+    modifier onlyDAO() {
+        require(msg.sender == dao, "only for Dao");
+        _;
+    }
+    modifier onlyMember() {
+        require(memberJoinDate[msg.sender] != 0, "only for cohort members");
+        _;
+    }
+
+    function join() public onlyDAO {
+        require(members.length <= 7, "cohort is full");
+        members.push(msg.sender);
+        memberJoinDate[msg.sender] = block.timestamp;
+    }
+
+    function leave(address _subject) public onlyDAO {
+        require(memberJoinDate[_subject] != 0, "is not a member");
+        require(memberJoinDate[_subject] + 90 days < block.timestamp, "00 days minimum");
+        delete memberJoinDate[_subject];
+        uint256 i = 0;
+        uint256 membersLen = members.length;
+        for (i; i < membersLen; i++) {
+            if (members[i] == _subject) {
+                delete members[i];
+                if (i != membersLen - 1) {
+                    members[i] = members[membersLen - 1];
+                }
+                break;
+            }
+        }
+    }
+
+    // set meeting day
+    // set cohort join date per member
+    // create meeting statement
+    // get reward for meeting with all members
+
+    function getMembers() public view returns (address[] memory) {
+        return members;
+    }
+
+    function createSession() public onlyMember {
+
     }
 }
--- contracts/UpliftDAO.sol
+++ contracts/UpliftDAO.sol
@@ -2,16 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "./tokens/UpliftNFT.sol";
+import "./Cohort.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract UpliftDAO {
 
     address public nftAddress;
     address[] public cohortsArr;
-    mapping(address => uint256) cohortStatus;
+    mapping(address => uint256) public cohortStatus;
+    uint256 public cohortsCount;
+    mapping(address => uint256) public cohortMembers;
+    address[] public cohortMemebrsArr;
+    mapping(uint256 => address) public cohortIdToAddress;
 
     constructor(string memory baseURI) {
         nftAddress = new UpliftNFT(baseURI);
+        cohortsCount = 0;
     }
 
     modifier memberOnly() {
@@ -19,9 +25,20 @@ contract UpliftDAO {
         _;
     }
 
-    function createCohort(string calldata mission) public memberOnly {
-        address memory cohort = New Cohort(mission);
+    function createCohort(string calldata name, string calldata mission) public memberOnly {
+        require(cohortMembers[msg.sender].length == 0, "already a member");
+        address cohortAddr = new Cohort(cohortsCount, cohortId, name, mission, msg.sender);
+        cohortIdToMembers[cohortsCount] = [cohortAddr];
+        cohortsCount++;
     }
 
+    function joinCohort(uint256 _cohortId) public memberOnly {
+        require(cohortMembers[msg.sender].length == 0, "already a member");
+        ICohort(cohortIdToAddress[_cohortId]).join(msg.sender);
+    }
 
+    function leaveCohort(uint256 _cohortId) public memberOnly {
+        require(cohortMembers[msg.sender].length > 0, "not a cohort member");
+        ICohort(cohortIdToAddress[_cohortId]).leave(msg.sender);
+    }
 }
